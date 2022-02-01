1eb14d005bf8e775bdff46b37dd62b1bc133c684
mattkatz <mattkatz@morelightmorelight.com>
2022-02-01 18:23:24 -0500

    add argparse as default cli

--- {{cookiecutter.project_slug}}/{{cookiecutter.pkg_name}}/cli.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.pkg_name}}/cli.py
@@ -1,17 +1,37 @@
-"""Console script for {{cookiecutter.pkg_name}}."""
+"""Console script for {{cookiecutter.project_slug}}."""
 
-{% if cookiecutter.command_line_interface|lower == 'click' -%}
+{%- if cookiecutter.command_line_interface|lower == 'argparse' %}
+import argparse
+{%- endif %}
+import sys
+{%- if cookiecutter.command_line_interface|lower == 'click' %}
 import click
+{%- endif %}
 
-
+{% if cookiecutter.command_line_interface|lower == 'click' %}
 @click.command()
-def main():
-    """Main entrypoint."""
-    click.echo("{{ cookiecutter.project_slug }}")
-    click.echo("=" * len("{{ cookiecutter.project_slug }}"))
-    click.echo("{{ cookiecutter.project_short_description }}")
+def main(args=None):
+    """Console script for {{cookiecutter.project_slug}}."""
+    click.echo("Replace this message by putting your code into "
+               "{{cookiecutter.project_slug}}.cli.main")
+    click.echo("See click documentation at https://click.palletsprojects.com/")
+    return 0
+{%- endif %}
+{%- if cookiecutter.command_line_interface|lower == 'argparse' %}
+def main(testargs=None):
+    """Console script for {{cookiecutter.project_slug}}."""
+    if not testargs:
+        testargs = sys.argv[1:]
+    parser = argparse.ArgumentParser(description="{{cookiecutter.project_short_description}}")
+    parser.add_argument('_', nargs='*')
+    args = parser.parse_args(testargs)
+
+    print("Arguments: " + str(args._))
+    print("Replace this message by putting your code into "
+          "{{cookiecutter.project_slug}}.cli.main")
+    return 0
+{%- endif %}
 
 
 if __name__ == "__main__":
-    main()  # pragma: no cover
-{%- endif %}
+    sys.exit(main())  # pragma: no cover
